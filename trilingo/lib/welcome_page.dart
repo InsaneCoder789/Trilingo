01726ad0ea0d8c54ce07ec0f45fa3a6d1d6d56eb
InsaneCoder789 <chatterjeerohan11@gmail.com>
2023-09-03 21:54:35 +0400

    A New and Modified Home Introduction Added

--- trilingo/lib/welcome_page.dart
+++ trilingo/lib/welcome_page.dart
@@ -15,12 +15,14 @@ class _Onboarding1WidgetState extends State<Onboarding1Widget> {
   void initState() {
     super.initState();
     _controller = VideoPlayerController.asset(
-      'assets/animations/main_animation.mp4', // Replace with the correct asset path
-    )..initialize().then((_) {
-        setState(() {});
-      });
-    _controller.setLooping(true);
-    _controller.play();
+      'assets/animations/main4.mp4', // Replace with the correct asset path
+    )
+      ..initialize().then((_) {
+        setState(() {
+          _controller.play(); // Play the video when it's initialized
+        });
+      })
+      ..setLooping(true); // Set looping to true to loop the video
   }
 
   @override
@@ -35,133 +37,127 @@ class _Onboarding1WidgetState extends State<Onboarding1Widget> {
       backgroundColor: Colors.white,
       body: Column(
         children: [
+          SizedBox(
+              height: 60), // Added some space between the status bar and text
           Expanded(
-            child: Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Color(0xFF6A57FF), // Purple
-                        Color(0xFFFF5963), // Red
-                      ],
-                      stops: [0.2, 1.0],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 120,
+                    height: 155,
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Image.asset(
+                      'assets/images/welcome_image.png',
+                      width: 120,
+                      height: 120,
+                      fit: BoxFit.cover,
                     ),
                   ),
-                ),
-                _controller.value.isInitialized
-                    ? AspectRatio(
-                        aspectRatio: _controller.value.aspectRatio,
-                        child: VideoPlayer(_controller),
-                      )
-                    : Container(), // Display a loading indicator while the video is loading
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 160,
-                        height: 160,
-                        decoration: BoxDecoration(
-                          color: Colors.transparent,
-                          shape: BoxShape.circle,
-                        ),
-                        child: Image.asset(
-                          'assets/images/welcome_image.png',
-                          width: 100,
-                          height: 100,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      SizedBox(height: 44),
-                      Text(
-                        'Welcome!',
-                        style: TextStyle(
-                          fontFamily: 'Plus Jakarta Sans',
-                          color: Colors.white,
-                          fontSize: 36,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      SizedBox(height: 8),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                        child: Text(
-                          'Thanks for joining! Enjoy the new era of Tourism with all-new Trilingo.',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontFamily: 'Plus Jakarta Sans',
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ),
-                    ],
+                  SizedBox(height: 0),
+                  Text(
+                    'TRILINGO',
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      color: Color(0xFF6A57FF),
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-              ],
+
+                  SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Text(
+                      'Enjoy the new era of Tourism with all-new Trilingo.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Plus Jakarta Sans',
+                        color: Color(0xFF6A57FF), // Purple
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 1), // Adjusted spacing
+                ],
+              ),
             ),
           ),
+          SizedBox(height: 10), // Added some space between text and video
+          Expanded(
+            flex: 1, // Adjusted the flex value to make the video larger
+            child: AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: VideoPlayer(_controller),
+            ),
+          ),
+          SizedBox(height: 20), // Adjusted spacing for buttons
           Padding(
-            padding: EdgeInsets.fromLTRB(16, 24, 16, 44),
-            child: Row(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(
-                          builder: (context) => SignupPage(),
-                        ),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xFF6A57FF), // Purple
-                      minimumSize: Size(230, 52),
-                      textStyle: TextStyle(
-                        fontFamily: 'Plus Jakarta Sans',
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
+                ElevatedButton(
+                  onPressed: () async {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (context) => SignupPage(),
                       ),
-                      elevation: 3,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xFF6A57FF), // Purple
+                    minimumSize: Size(300, 62),
+                    textStyle: TextStyle(
+                      fontFamily: 'Plus Jakarta Sans',
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
                     ),
-                    child: Text('Get Started'),
+                    elevation: 20,
+                    shadowColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
                   ),
+                  child: Text('Get Started'),
                 ),
-                SizedBox(width: 8),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(
-                          builder: (context) => LoginPage(),
-                        ),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.white,
-                      onPrimary: Color(0xFF6A57FF), // Purple
-                      minimumSize: Size(230, 52),
-                      textStyle: TextStyle(
-                        fontFamily: 'Plus Jakarta Sans',
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
+                SizedBox(
+                  height: 25, // Adjusted height
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (context) => LoginPage(),
                       ),
-                      elevation: 3,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white,
+                    onPrimary: Color(0xFF6A57FF), // Purple
+                    minimumSize: Size(300, 62),
+                    textStyle: TextStyle(
+                      fontFamily: 'Plus Jakarta Sans',
+                      color: Color(0xFF6A57FF), // Purple
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
                     ),
-                    child: Text('My Account'),
+                    elevation: 15,
+                    shadowColor: Color(0xFF6A57FF),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
                   ),
+                  child: Text('My Account'),
+                ),
+                SizedBox(
+                  height: 45, // Adjusted height
                 ),
               ],
             ),
