810bd520a272752a6f1261504524f58727cd6ab8
RochieeKIIT <24155866@kiit.ac.in>
2025-07-04 21:45:01 +0400

    SpacethemeUi shifted to newfile for Multiple Usage

--- trilingo/lib/home_widgets/hotel_bookings_page.dart
+++ trilingo/lib/home_widgets/hotel_bookings_page.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 import 'dart:ui';
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:crypto/crypto.dart';
+import 'package:trilingo/ui/widgets/spacethemeui.dart';
 
 class HotelBookingsPage extends StatefulWidget {
   @override
@@ -409,92 +409,6 @@ class _HotelBookingsPageState extends State<HotelBookingsPage> {
   }
 }
 
-
-class AnimatedSpaceBackground extends StatefulWidget {
-  const AnimatedSpaceBackground({super.key});
-
-  @override
-  State<AnimatedSpaceBackground> createState() => _AnimatedSpaceBackgroundState();
-}
-
-class _AnimatedSpaceBackgroundState extends State<AnimatedSpaceBackground> with SingleTickerProviderStateMixin {
-  late final AnimationController _controller;
-  late final List<_Star> _stars;
-
-  @override
-  void initState() {
-    super.initState();
-    _stars = List.generate(150, (index) => _Star.random());
-    _controller = AnimationController(vsync: this, duration: const Duration(seconds: 10))..repeat();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _controller,
-      builder: (context, child) {
-        return CustomPaint(
-          painter: _StarPainter(_stars, _controller.value),
-          child: Container(),
-        );
-      },
-    );
-  }
-}
-
-class _Star {
-  Offset position;
-  double radius;
-  Color color;
-  double twinkleSpeed;
-
-  _Star({
-    required this.position,
-    required this.radius,
-    required this.color,
-    required this.twinkleSpeed,
-  });
-
-  factory _Star.random() {
-    final random = Random();
-    return _Star(
-      position: Offset(random.nextDouble(), random.nextDouble()),
-      radius: random.nextDouble() * 1.5 + 0.5,
-      color: Colors.white,
-      twinkleSpeed: random.nextDouble() * 2 + 1,
-    );
-  }
-}
-
-class _StarPainter extends CustomPainter {
-  final List<_Star> stars;
-  final double animationValue;
-
-  _StarPainter(this.stars, this.animationValue);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint();
-    for (var star in stars) {
-      final alpha = (0.5 + 0.5 * sin(animationValue * 2 * pi * star.twinkleSpeed)) * 255;
-      paint.color = star.color.withAlpha(alpha.toInt());
-      canvas.drawCircle(
-        Offset(star.position.dx * size.width, star.position.dy * size.height),
-        star.radius,
-        paint,
-      );
-    }
-  }
-
-  @override
-  bool shouldRepaint(covariant _StarPainter oldDelegate) => true;
-}
 
 class HotelApiService {
   static const apiKey = 'ccd37e61d6b6b205c736e05cc670b7e9';
